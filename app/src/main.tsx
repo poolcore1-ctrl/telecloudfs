6f57d8b42349cbc2d6b25a827e9cc39bffa69305
poolcore1-ctrl <poolcore1@gmail.com>
2026-05-05 23:02:57 +0530

    Explicit polyfills in main.tsx for GramJS

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
 
+// Critical polyfills for GramJS
+import { Buffer } from 'buffer';
+import process from 'process';
+import * as util from 'util';
+
+(window as any).global = window;
+(window as any).Buffer = Buffer;
+(window as any).process = process;
+(window as any).util = util;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
